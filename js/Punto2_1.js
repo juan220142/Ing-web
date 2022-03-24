d4051fdf3a5483ca203489287719a0831433bad1
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2022-03-24 14:03:06 -0500

    Datos_Vis2

---
+++ js/Punto2_1.js
@@ -0,0 +1,129 @@
+let body = d3.select("#body")
+let container = d3.select("#container")
+
+d3.csv("./doc/data3.csv").then((data) => {
+    showData(data);
+})
+function showTooltip(text, coords) {
+    let x = coords[0];
+    let y = coords[1];
+
+    d3.select("#tooltip")
+    .style("display", "block")
+    .style("top", y)
+    .style("left", x)
+    .text(text)
+}
+function showData(clients) {
+
+
+    let bodyWidth = 450;
+    let bodyHeight = 450;
+    let xExtent = d3.extent(clients, d => +d.BIRTH_pc5)
+    let xScale = d3.scaleLinear().range([0, bodyWidth])
+        .domain([0, xExtent[1] + 30])
+
+
+    let yExtent = d3.extent(clients, d => +d.BIRTH_talla5)
+    let yScale = d3.scaleLinear().range([bodyHeight, 0])
+        .domain([0, yExtent[1] + 30])
+
+    let joinCircle = body.selectAll("circle")
+        .data(clients.filter(d=>d.Pretermino == 0))
+
+    let newelementsCircle = joinCircle.enter()
+        .append("circle")
+        .style("fill", "green")
+        .style("opacity","0.5")
+        .style("r", "3")
+
+
+    let joinRect = body.selectAll("rect")
+        .data(clients.filter(d=>(d.Pretermino == 1)))
+
+    let newelementsRect = joinRect.enter()
+        .append('rect')
+        .attr('width', 5)
+        .attr('height', 5)
+        .style("opacity","0.5")
+        .attr('fill', 'gray')
+
+    joinRect.merge(newelementsRect)
+        .transition()
+        .attr("x", d => xScale(+d.BIRTH_pc5))
+        .attr("y", d => yScale(+d.BIRTH_talla5))
+
+
+    joinCircle.merge(newelementsCircle)
+        .transition()
+        .attr("cx", d => xScale(+d.BIRTH_pc5))
+        .attr("cy", d => yScale(+d.BIRTH_talla5))
+
+
+    let yAxis = d3.axisLeft(yScale);
+    let yAxisGroup = d3.select("#yAxis")
+        .style("transform", "translate(40px, 10px)")
+        .call(yAxis)
+
+    let xAxis = d3.axisBottom(xScale)
+    let xAxisGroup = d3.select("#xAxis")
+        .style("transform", `translate(40px, ${bodyHeight + 10}px)`)
+        .call(xAxis)
+
+        let zoom = d3.zoom()
+    zoom.on("zoom", function (a, b) {
+        let newXScale = d3.event.transform.rescaleX(xScale);
+        let newYScale = d3.event.transform.rescaleY(yScale);
+
+        xAxis.scale(newXScale)
+        xAxisGroup.call(xAxis)
+
+        yAxis.scale(newYScale)
+        yAxisGroup.call(yAxis)
+
+            joinCircle.merge(newelementsCircle)
+            .attr("cx", d => newXScale(+d.BIRTH_pc5))
+            .attr("cy", d => newYScale(+d.BIRTH_talla5))
+            .on("mouseenter", (d) => {
+            let dat= "Hombre"
+            if(d.BIRTH_sexo5==2){
+                dat="Mujer";
+            }
+            showTooltip(dat, [d3.event.clientX, d3.event.clientY])
+            })
+            .on("mousemove", (d) => {
+            let dat= "Hombre"
+            if(d.BIRTH_sexo5==2){
+                dat="Mujer";
+            }
+            showTooltip(dat, [d3.event.clientX, d3.event.clientY + 30])
+            })
+            .on("mouseleave", (d) => {
+            d3.select("#tooltip").style("display", "none")
+            })
+
+            joinRect.merge(newelementsRect)
+            .attr("x", d => newXScale(+d.BIRTH_pc5))
+            .attr("y", d => newYScale(+d.BIRTH_talla5))
+            .on("mouseenter", (d) => {
+            let dat= "Hombre"
+            if(d.BIRTH_sexo5==2){
+                dat="Mujer";
+            }
+            showTooltip(dat, [d3.event.clientX, d3.event.clientY])
+            })
+            .on("mousemove", (d) => {
+            let dat= "Hombre"
+            if(d.BIRTH_sexo5==2){
+                dat="Mujer";
+            }
+            showTooltip(dat, [d3.event.clientX, d3.event.clientY + 30])
+            })
+            .on("mouseleave", (d) => {
+            d3.select("#tooltip").style("display", "none")
+            })
+
+    });
+    container.call(zoom)
+
+}
